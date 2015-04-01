1c739c9055f5e32e308d28449cbf5f6200a04cff
Jesse MacFadyen <purplecabbage@gmail.com>
2015-03-31 17:09:02 -0700

    Add a callback to get the results from the now modular version of paramedic.js

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ var plugins,
     platformId;
 
 var USAGE = "Error missing args. \n Usage: $cordova-paramedic " +
-            "  --platform CORDOVA-PLATFORM --plugin PLUGIN-PATH [--port PORT]";    
+            "  --platform CORDOVA-PLATFORM --plugin PLUGIN-PATH [--justbuild ] [--port PORT] [--timeout TIMEOUT] \n" +
+            " CORDOVA-PLATFORM : Which platform to build target\n" +
+            " PLUGIN-PATH : Path to the plugin to install.\n" +
+            " --justbuild : Will not run tests, just verify that the plugin+platform can be built.\n" +
+            " TIMEOUT : time to wait for tests to run, in msecs, default is 10 minutes."; 
 
 var argv = parseArgs(process.argv.slice(2));
 
@@ -20,9 +24,11 @@ platformId = argv.platform;
 plugins = argv.plugin;
 plugins = Array.isArray(plugins) ? plugins : [plugins];
 
+var onComplete = function(resCode,resObj) {
+	console.log("result is : " + resCode + "\n" + JSON.stringify(resObj));
+	process.exit(resCode);
+}
 
-paramedic.run(platformId,plugins,argv.justbuild,argv.port,argv.timeout);
 
-
-
+paramedic.run(platformId, plugins, onComplete, argv.justbuild, argv.port, argv.timeout);
 
