f88d8d61d5d10296ac1cceb73bbfb8ed3e6eb2b8
Tomasz Subik <tsubik@gmail.com>
2015-06-02 19:01:18 +0200

    verbose mode available

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ var USAGE = "Error missing args. \n" +
 			  "\t(defaults to 10 minutes) \n" +
 	"`PORTNUM` : (optional) port to use for posting results from emulator back to paramedic server\n" +
 	"--justbuild : (optional) just builds the project, without running the tests \n" +
-    "--browserify : (optional) plugins are browserified into cordova.js";
+    "--browserify : (optional) plugins are browserified into cordova.js \n" +
+    "--verbose : (optional) verbose mode. Display more information output";
 
 var argv = parseArgs(process.argv.slice(2));
 
@@ -37,5 +38,5 @@ var onComplete = function(resCode,resObj,logStr) {
 	process.exit(resCode);
 };
 
-paramedic.run(argv.platform, argv.plugin, onComplete, argv.justbuild, argv.port, argv.timeout, argv.browserify, false);
+paramedic.run(argv.platform, argv.plugin, onComplete, argv.justbuild, argv.port, argv.timeout, argv.browserify, false, argv.verbose);
 
